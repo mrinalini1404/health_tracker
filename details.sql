69b23a049c99fea713aac517850bbf3a2ff8b808
thanatos248 <nibbler@nibbler.localdoman>
2020-11-12 02:22:43 +0530

    Articular Gas

--- details.sql
+++ details.sql
@@ -61,7 +61,7 @@ CREATE TABLE `details` (
   `timestamp` datetime DEFAULT NULL,
   KEY `id` (`id`),
   CONSTRAINT `details_ibfk_1` FOREIGN KEY (`id`) REFERENCES `accounts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
